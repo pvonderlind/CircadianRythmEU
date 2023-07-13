2029d4fd647c66d608b6d39fe4eb6bba694a590d
Philip Vonderlind <philipv@hotmail.de>
2023-07-13 12:38:55 +0200

    Update index.py to new version

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
+import numpy as np
 from bokeh.plotting import figure
-from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, ColumnDataSource, LabelSet, HoverTool
+from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, ColumnDataSource, LabelSet, HoverTool, \
+    CategoricalColorMapper
 from bokeh.tile_providers import Vendors
 from bokeh.models import DataTable, TableColumn
 from bokeh.palettes import Plasma256
@@ -13,14 +15,15 @@ import pandas as pd
 eu_data_path = 'https://raw.githubusercontent.com/pvonderlind/CircadianRythmEU/master/datasets/saved/eu_gpd.geojson'
 city_data_path = 'https://raw.githubusercontent.com/pvonderlind/CircadianRythmEU/master/datasets/saved/city_data.csv'
 sunset_data = 'https://raw.githubusercontent.com/pvonderlind/CircadianRythmEU/master/datasets/saved/sunset_data_2022.geojson'
-avg_country_data =  'https://raw.githubusercontent.com/pvonderlind/CircadianRythmEU/master/datasets/saved/avg_country_data.csv'
+avg_country_data = 'https://raw.githubusercontent.com/pvonderlind/CircadianRythmEU/master/datasets/saved/avg_country_data.csv'
 
 eu_gpd = gpd.read_file(eu_data_path)
 top_city_data = pd.read_csv(city_data_path)
 sun_data_gpd = gpd.read_file(sunset_data)
+avg_country_data = pd.read_csv(avg_country_data)
 
-data_field = 'sunrise_UTC'
-bokeh_tools = 'wheel_zoom, pan, box_zoom, reset'
+data_field = 'social_timezone'
+bokeh_tools = 'wheel_zoom, pan, box_zoom, reset, save'
 colorbar_settings = {'title_text_font_size': '12pt', 'label_standoff': 12}
 
 
@@ -37,59 +40,67 @@ def bokeh_plot_map(data):
     p.xgrid.grid_line_color = None
     p.ygrid.grid_line_color = None
 
-    # AD MAP TILES ---------------------------------------------------------------------------
+    # ADD MAP TILES ---------------------------------------------------------------------------
     p.add_tile(Vendors.CARTODBPOSITRON_RETINA)
+
+    # ADD TIMEZONE LINES
 
     # ADD GEO STUFF FOR COUNTRIES AS A WHOLE -------------------------------------------------
     geo_data_source = get_bokeh_geodata_source(data)
 
     values = data[data_field]
-    palette = brewer['OrRd'][8]
+    palette = brewer['OrRd'][3]
     palette = palette[::-1]
-    # Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
-    color_mapper = LinearColorMapper(palette=palette, low=values.min(), high=values.max())
-    color_bar = ColorBar(color_mapper=color_mapper, location=(0, 0), title='Sunrise (UTC)', **colorbar_settings)
-    country_suntimes = p.patches('xs', 'ys', source=geo_data_source,
-                                 fill_color={'field': data_field, 'transform': color_mapper},
-                                 line_color='blue',
-                                 line_width=0.5,
-                                 fill_alpha=0.8)
+    color_mapper = CategoricalColorMapper(palette=palette, factors=values.unique().tolist())
+    color_bar = ColorBar(color_mapper=color_mapper, location=(0, 0), title='Timezone', **colorbar_settings)
+    country_tz = p.patches('xs', 'ys', source=geo_data_source,
+                           fill_color={'field': data_field, 'transform': color_mapper},
+                           line_color='blue',
+                           line_width=0.5,
+                           fill_alpha=0.8)
     p.add_layout(color_bar, 'below')
 
     # TOOLTIPS FOR COUNTRY PATCHES
     tooltips_country = [
-        ('Country', '@iso_a3'),
-        ('Sunrise (UTC)', '@sunrise_UTC'),
-        ('Sunset (UTC)', '@sunset_UTC')
+        ('Country', '@iso_a2')
     ]
-    p.add_tools(HoverTool(renderers=[country_suntimes], tooltips=tooltips_country))
+    p.add_tools(HoverTool(renderers=[country_tz], tooltips=tooltips_country))
 
     # ===================================================================================================================
 
-    # ADD TIMEZONE INFO PER TOP N CITIES FOR EACH COUNTRY ------------------------------------
-
-    city_data_source = ColumnDataSource(data=top_city_data)
-    city_color_mapper = LinearColorMapper(palette=Plasma256,
-                                          low=top_city_data['longitudinal_diff_km'].min(),
-                                          high=top_city_data['longitudinal_diff_km'].max())
-    city_color_bar = ColorBar(color_mapper=city_color_mapper, location=(0, 0),
-                              title="Distance to east timezone meridian (Km)", **colorbar_settings)
-    city_longdiff_circles = p.circle(x='mercantor_x', y='mercantor_y', source=city_data_source,
-                                     color={'field': 'longitudinal_diff_km', 'transform': city_color_mapper}, size=10,
-                                     fill_alpha=1)
-    labels = LabelSet(x='mercantor_x', y='mercantor_y', x_offset=5, y_offset=5, text='NAME', source=city_data_source,
-                      text_color='cornflowerblue')
-
-    p.add_layout(labels)
-    p.add_layout(city_color_bar, 'right')
+    # ADD BARS FOR DISTANCE TO EAST MERIDIAN EFFECT
+    length_scale = 200000
+    bar_data_source = ColumnDataSource(dict(
+        x0=avg_country_data['mercantor_x'],
+        y0=avg_country_data['mercantor_y'],
+        x1=avg_country_data['mercantor_x'] + (length_scale * avg_country_data['norm_weighted_mean_longdiff']) * np.sign(
+            avg_country_data['weighted_mean_longdiff']),
+        y1=avg_country_data['mercantor_y'],
+        long_diff=avg_country_data['mean_longitudinal_diff_km'],
+        weighted_long_diff=avg_country_data['weighted_mean_longdiff'],
+        long_diff_norm=avg_country_data['norm_weighted_mean_longdiff']
+    )
+    )
+    divider_len = 50000
+    divider_data_source = ColumnDataSource(dict(
+        x0=avg_country_data['mercantor_x'],
+        y0=avg_country_data['mercantor_y'] - divider_len / 2,
+        x1=avg_country_data['mercantor_x'],
+        y1=avg_country_data['mercantor_y'] + divider_len / 2,
+        text=avg_country_data['name'],
+        text_y=(avg_country_data['mercantor_y'] + divider_len / 2) + 1000
+    ))
+    longdiff_quads = p.segment(x0="x0", y0="y0", x1="x1", y1="y1", line_width=5, source=bar_data_source)
+    londiff_diviers = p.segment(x0="x0", y0="y0", x1="x1", y1="y1", line_width=2, source=divider_data_source)
+    longdiff_label = p.text(x="x0", y="text_y", text="text", source=divider_data_source)
 
     # TOOLTIPS FOR CITY DATA
-    tooltips_city = [
-        ('Country', '@country_ISO_A2'),
-        ('City', '@NAME'),
-        ('Relative position (dist) to timezone border', '@longitudinal_diff_km')
+    tooltips_longquads = [
+        ('Relative distance (km) to timezone border', '@long_diff'),
+        ('Weighted (population size) diff. to east timezone border', '@weighted_long_diff'),
+        ('Normalized, weighted (population size) effect of distance to east meridian', '@long_diff_norm')
     ]
-    p.add_tools(HoverTool(renderers=[city_longdiff_circles], tooltips=tooltips_city, name='test'))
+    p.add_tools(HoverTool(renderers=[longdiff_quads], tooltips=tooltips_longquads, name='long_quads'))
     return p
 
 
@@ -100,17 +111,19 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 def bokeh_country_table(country_data):
-    country_data_sorted = country_data.sort_values('weighted_mean_longdiff', ascending=False)
+    country_data_sorted = country_data.sort_values('norm_weighted_mean_longdiff', ascending=False)
     source = ColumnDataSource(country_data_sorted)
     columns = [
         TableColumn(field='name', title='Country Name'),
-        TableColumn(field="iso_a2",title="Country Code (ISO_A2)"),
+        TableColumn(field="iso_a2", title="Country Code (ISO_A2)"),
         TableColumn(field="social_timezone", title="Social Timezone"),
-        TableColumn(field="weighted_mean_longdiff", title="Weighted (pop.size) avg. dist. to east meridian (km)"),
+        TableColumn(field="norm_weighted_mean_longdiff", title="Normalized weighted effect of dist. to east meridian"),
+        TableColumn(field="mean_longitudinal_diff_km", title="Avg. dist. to east meridian (km)"),
         TableColumn(field="pop_est", title="Estimated population")
     ]
     data_table = DataTable(source=source, columns=columns)
     return data_table
+
 
 def bokeh_sun_table(sun_data):
     source = ColumnDataSource(sun_data)
@@ -132,13 +145,17 @@ def map_visualization():
     # CREATE MAP  ----------------------------------------------------------------------------------
     # Create Map Panel
     map_pane = pn.pane.Bokeh(sizing_mode='scale_both', width_policy='max')
-    start_date = date(2022,1,1)
-    end_date = date(2022,12,31)
+    start_date = date(2022, 1, 1)
+    end_date = date(2022, 12, 31)
     selected_date = pn.widgets.DateSlider(name='Date Slider', value=start_date, start=start_date, end=end_date)
+
+    eu_geo_tz = eu_gpd.merge(avg_country_data)
+
     def update_map(event):
         d = selected_date.value
         selected_sundata = sun_data_gpd.query(f'day == {d.day} & month == {d.month} & year == {d.year}')
-        map_pane.object = bokeh_plot_map(selected_sundata)
+        map_pane.object = bokeh_plot_map(eu_geo_tz)
+
     selected_date.param.watch(update_map, 'value')
     selected_date.param.trigger('value')
 
@@ -150,12 +167,13 @@ def map_visualization():
 
     # Create Sun Table Panel
     sun_data_pane = pn.pane.Bokeh(**sizing_dict)
-    sun_data_pane.object = bokeh_sun_table(sun_data_gpd.iloc[:,:-1])
+    sun_data_pane.object = bokeh_sun_table(sun_data_gpd.iloc[:, :-1])
 
     # Create panel application layout
     map_vis = pn.Column(selected_date, map_pane)
     tabs = pn.Tabs(('Map', map_vis), ('Country Data', country_data_pane), ('Sun Data', sun_data_pane))
     return tabs
+
 
 # SERVE APP
 app = map_visualization()
